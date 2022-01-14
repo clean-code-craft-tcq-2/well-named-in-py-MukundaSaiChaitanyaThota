fb5613f842c8adee931c33689b3d22f0394fe31c
MukundaSaiChaitanyaThota <91048236+MukundaSaiChaitanyaThota@users.noreply.github.com>
2022-01-14 20:27:23 +0530

    Update reference_manual.py

--- reference_manual.py
+++ reference_manual.py
@@ -1,12 +1,13 @@
 from pair_color_transition import *
 
 def color_manual():
-        pair_no = 1
-        print("Pair_Number" ,color_pair_to_string("Major_color","Minor_Color"))
-        for color1 in MAJOR_COLORS:
-            for color2 in MINOR_COLORS:
-                print(pair_no,color_pair_to_string(color1,color2))
-                pair_no += 1
+        pair_no=1
+        print("**************Reference Manual *****************")
+        print("Pair_No.|Major_color\t|Minor_Color")
+        for major_color in MAJOR_COLORS:
+            for minor_color in MINOR_COLORS:
+                print(f'{pair_no}\t|{color_pair_to_string(major_color,minor_color)}')
+                pair_no+=1
 
 def color_pair_to_string(major_color, minor_color):
-   return f'{major_color} {minor_color}'
+   return f'{major_color}\t\t|{minor_color}'
