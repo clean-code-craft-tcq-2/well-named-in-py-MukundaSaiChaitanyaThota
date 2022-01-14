c777cf890ece0668832e1b65a0be8aed39e3a281
MukundaSaiChaitanyaThota <91048236+MukundaSaiChaitanyaThota@users.noreply.github.com>
2022-01-14 14:58:16 +0530

    Update pair_color_transition.py

--- pair_color_transition.py
+++ pair_color_transition.py
@@ -1,8 +1,5 @@
-
 MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
 MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
-
-
 
 def get_color_from_pair_number(pair_number):
   zero_based_pair_number = pair_number - 1
@@ -13,7 +10,6 @@ def get_color_from_pair_number(pair_number):
   if minor_index >= len(MINOR_COLORS):
     raise Exception('Minor index out of range')
   return MAJOR_COLORS[major_index], MINOR_COLORS[minor_index]
-
 
 def get_pair_number_from_color(major_color, minor_color):
   try:
