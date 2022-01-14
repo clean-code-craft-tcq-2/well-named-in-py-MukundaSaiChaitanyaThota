213f626a7c2f888c7048bbe954e3808b32914f39
MukundaSaiChaitanyaThota <91048236+MukundaSaiChaitanyaThota@users.noreply.github.com>
2022-01-14 14:56:25 +0530

    Update reference_manual.py

--- reference_manual.py
+++ reference_manual.py
@@ -1,13 +1,12 @@
 from pair_color_transition import *
 
 def color_manual():
-        pair_no=1
+        pair_no = 1
         print("Pair_Number" ,color_pair_to_string("Major_color","Minor_Color"))
         for color1 in MAJOR_COLORS:
             for color2 in MINOR_COLORS:
                 print(pair_no,color_pair_to_string(color1,color2))
-                pair_no+=1
-
+                pair_no += 1
 
 def color_pair_to_string(major_color, minor_color):
    return f'{major_color} {minor_color}'
